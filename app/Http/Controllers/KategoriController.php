22fab6a6daaddc32327a151b7e1c22c3a3270f3f
EvanderSG29 <smidgidionevander@gmail.com>
2025-09-22 11:09:41 +0700

    new file:   app/Http/Controllers/KategoriController.php 	new file:   app/Http/Requests/KategoriStoreRequest.php 	new file:   app/Http/Requests/KategoriUpdateRequest.php 	new file:   app/Models/Kategori.php 	new file:   resources/views/kategoris/create.blade.php 	new file:   resources/views/kategoris/edit.blade.php 	new file:   resources/views/kategoris/index.blade.php 	new file:   resources/views/kategoris/show.blade.php 	new file:   resources/views/layouts/main.blade.php

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $kategoris = Kategori::latest()->paginate(5);
+        return view ('kategoris.index', compact ('kategoris'))
+        ->with('i',(request()->input('page',1)-2)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('kategoris.create'); 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kategori $kategori)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kategori $kategori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Kategori $kategori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kategori $kategori)
+    {
+        //
+    }
+}
